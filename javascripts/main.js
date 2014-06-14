17d9a5ab708a7bdad706e263b26f430528bb34f1
appostolov <appostolov@gmail.com>
2014-06-15 01:28:59 +0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,39 +3,17 @@
 var v1 = document.getElementById("can");
 var canvas = v1.getContext("2d");
 
-var x = v1.width/2;
-var y = v1.height/2;
+var x = document.getElementById("x");
+var y = document.getElementById("y");
+var rad = document.getElementById("rad");
+var color = document.getElementById("color");
 
-var r = 200;
-
-var op = 1;
-var color = "0,0,255,";
-
-canvas.fillStyle = "#cc9";
-canvas.fillRect(0,0,v1.width,v1.height);
-
-canvas.fillStyle = "#000";
-canvas.fillRect(x,y,2,2);
-
-var u = x+r;
-var v = y;
-
-var int = setInterval(function(){
+var paint = function(){
+		canvas.fillStyle = color.value;
+		canvas.beginPath();
+		canvas.arc(x.value,y.value,rad.value,0,2*Math.PI);
+		canvas.fill();
+};
 	
-		canvas.fillStyle = "rgba("+color+op+")";
-		canvas.fillRect(u,v,2,2);
 		
-		if(v<=y){
-			u -= 1;
-			v = y - Math.sqrt(Math.pow(r,2)-Math.pow(u-x,2));
-		}
 		
-		if(v>=y){
-			u += 1;
-			v = y + Math.sqrt(Math.pow(r,2)-Math.pow(u-x,2));
-			if(u>x+r-1){
-				clearInterval(int);
-			}
-		}
-		
-}, 17);
